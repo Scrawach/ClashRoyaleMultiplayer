ea0fa866edae6002317cb97c91d413bca82cdf77
scrawach <scrawach@gmail.com>
2024-04-16 15:58:06 +0300

    a little bit changes in state

--- client/ClashRoyaleMultiplayer/Assets/Scripts/State.cs
+++ client/ClashRoyaleMultiplayer/Assets/Scripts/State.cs
@@ -1,4 +1,12 @@
-public class State
+public class State : IState
 {
-    
+    public void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
 }
