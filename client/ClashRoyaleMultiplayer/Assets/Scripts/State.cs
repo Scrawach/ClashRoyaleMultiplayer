0ea65f8564c0e658ffc58e9288c6fb96032e6dc4
scrawach <scrawach@gmail.com>
2024-04-18 23:36:14 +0300

    removed throw exceptions on default state

--- client/ClashRoyaleMultiplayer/Assets/Scripts/State.cs
+++ client/ClashRoyaleMultiplayer/Assets/Scripts/State.cs
@@ -2,11 +2,9 @@ public class State : IState
 {
     public void Enter()
     {
-        throw new System.NotImplementedException();
     }
 
     public void Exit()
     {
-        throw new System.NotImplementedException();
     }
 }
