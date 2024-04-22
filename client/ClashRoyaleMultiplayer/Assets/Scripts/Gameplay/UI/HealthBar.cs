5d0d5a40940ad735c2b9137ca4ce8e686682b18a
scrawach <scrawach@gmail.com>
2024-04-23 01:42:36 +0300

    added health bar to tower and unit

---
+++ client/ClashRoyaleMultiplayer/Assets/Scripts/Gameplay/UI/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using Gameplay.Common;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Health _health;
+        [SerializeField] private Image _bar;
+
+        private void OnEnable() => 
+            _health.Changed += OnHealthChanged;
+
+        private void OnDisable() => 
+            _health.Changed -= OnHealthChanged;
+
+        private void OnHealthChanged() => 
+            _bar.fillAmount = _health.Ratio;
+    }
+}
