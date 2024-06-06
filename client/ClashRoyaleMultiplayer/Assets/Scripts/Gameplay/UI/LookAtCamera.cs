66bac37cc0f7b2092569a403727e535f32d7440d
scrawach <scrawach@gmail.com>
2024-06-06 11:17:02 +0300

    fixed look at camera script

--- client/ClashRoyaleMultiplayer/Assets/Scripts/Gameplay/UI/LookAtCamera.cs
+++ client/ClashRoyaleMultiplayer/Assets/Scripts/Gameplay/UI/LookAtCamera.cs
@@ -7,12 +7,15 @@ namespace Gameplay.UI
     {
         private Camera _camera;
 
-        private void Awake() => 
+        private void Start()
+        {
             _camera = Camera.main;
+        }
 
         private void Update()
         {
-            transform.LookAt(_camera.transform);
+            Quaternion rotation = _camera.transform.rotation;
+            transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
         }
     }
 }
